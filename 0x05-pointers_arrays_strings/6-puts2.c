f4ea0f95ffcd04ac03b6ceb9dadd20df37fdbef0
ziad <ziadreda512@gmail.com>
2023-07-25 08:24:35 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,4 @@
-/*#include "main.h"
-
-void puts2(char *str);
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; ++i)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
-	_putchar('\n');
-}*/
-
-#include <stdio.h>
+#include "main.h"
 
 void puts2(char *str) {
     if (str == NULL)
