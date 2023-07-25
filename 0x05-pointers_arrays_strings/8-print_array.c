c9e9da005999c969c81f5a7c0bbe8d45d1d80393
ziad <ziadreda512@gmail.com>
2023-07-25 08:38:39 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_array(int *a, int n) {
+    if (a == NULL || n <= 0)
+        return;
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", a[i]);
+
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
